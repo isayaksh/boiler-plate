854890c8a4eec7c6aff71f05db8db3d34184b97a
isayaksh <isayaksh@gmail.com>
2022-02-23 20:53:22 +0900

    Information Secure

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
 const express = require('express')
 const app = express()
-const port = 5000
+const port = 3000
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose')
+const {User} = require("./models/User");
+const config = require("./config/key");
 
-mongoose.connect("mongodb+srv://isayaksh:1UFdakINK9pJ35t0@boilerplate.pshaj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",{
+// application/x-www-form-urlencoded
+app.use(express.urlencoded({extended: true}));
+// application/json
+app.use(express.json());
+
+mongoose.connect(config.mongoURI,{
   useNewUrlParser: true, useUnifiedTopology: true
 }).then(()=> console.log("MongoDB Connected..."))
   .catch(err=>console.log(err))
 
 app.get('/', (req, res) => {
-  res.send('Hello World! 안녕하세요!')
+  res.send('Hello World! 안녕하살법!')
+})
+
+app.post('/register', (req, res) => {
+  // 회원 가입할 때 필요한 정보들을 client에서 가져오면 그것들을 데이터 베이스에 넣어준다.
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
 })
 
 app.listen(port, () => {
